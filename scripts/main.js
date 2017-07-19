aee540f7be7d5e9a7c98bfecc4f1bfa95f8687db
John Hartert <j.blainehartert@gmail.com>
2017-07-19 15:53:24 -0400

    Adds HTML

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+console.log('hello world');  
+document.getElementById('myBtn').addEventListener("click", displayNumber);
+function displayNumber() {
+    document.getElementById('field1').value = Math.floor(Math.random() * 69) + 1;
+    document.getElementById('field2').value = Math.floor(Math.random() * 69) + 1;
+    document.getElementById('field3').value = Math.floor(Math.random() * 69) + 1;
+    document.getElementById('field4').value = Math.floor(Math.random() * 69) + 1;
+    document.getElementById('field5').value = Math.floor(Math.random() * 69) + 1;
+    document.getElementById('field6').value = Math.floor(Math.random() * 26) + 1;
+}
+
+
+
+//Need to: Make a function for the onClick and nest all other functions that 
+//generate random numbers nested inside. 
+//Display each random generated number to each of the columns.
+//A way to ensure each number is unique. Most likely a function.
+//A function that attaches the event listener to the button
+//A function that writes the generated number to the page (most 
+//likely by assigning to the textContent property).
